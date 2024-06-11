e874b3faf151a054371614fc122e589767e97929
Minal Abayasekara <darkhorseminal@gmail.com>
2024-06-11 13:51:19 +0530

    Code  Added

---
+++ main.py
@@ -0,0 +1,13 @@
+# User enter two numbers
+n = int(input("Please enter the first number: "))
+m = int(input("Please enter the second number: "))
+
+
+print(n , m);
+print("Addition: ", n + m)
+print("Subtraction: ", n - m)
+print("Multiplication: ", n * m)
+print("Division: ", n / m)
+print("Exponential: ", n ** m)
+print("Modulus: ", n % m)
+print("Floor Division: ", n // m)
